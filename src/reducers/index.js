8f872a9a6a33cfa2621d2994629070446250cf98
xiehongwei <283994917@qq.com>
2019-08-12 18:15:01 +0800

    loadingBarReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import {combineReducers} from "redux";
 import authUser from "./authUser";
 import users from "./users";
 import questions from "./questions";
+import {loadingBarReducer} from "react-redux-loading";
 
 
 export default combineReducers({
     authUser,
     users,
-    questions
+    questions,
+    loadingBar: loadingBarReducer
 })
 
