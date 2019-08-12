21843fbee03730d77d36c3dcf27a48566fe78da2
xiehongwei <283994917@qq.com>
2019-08-12 17:54:57 +0800

    rootReducer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App'
+import {createStore} from "redux";
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 
-
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
 
