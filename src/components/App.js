21c8f19d407a5cc77524dcf73bb3892224cffc9c
xiehongwei <283994917@qq.com>
2019-08-12 18:31:03 +0800

    App ---> 初始数据

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,48 @@
-import React, {Component, Fragment} from 'react';
 // import logo from './logo.svg';
 // <img src={logo} className="App-logo" alt="logo" />
 
-
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import LoadingBar from 'react-redux-loading';
+import {handleInitData} from "../actions/shared";
+import Home from "./Home";
 
 
 class App extends Component {
 
+    componentDidMount() {
+        this.props.handleInitData();
+    }
 
-  render() {
-    return (
-        <div>
-          Start Code
-        </div>
-    );
-  }
+    render() {
+
+        const { loading } = this.props;
+
+        return (
+            <div>
+                <LoadingBar/>
+                {
+                    loading === true
+                        ? null
+                        : <Home/>
+
+                }
+            </div>
+        );
+    }
 
 }
 
 
-export default App;
+
+function mapStateToProps ({ authUser }) {
+    return {
+        loading: authUser === null
+    }
+}
+
+
+export default connect(
+    mapStateToProps,
+    { handleInitData }
+)(App);
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+
+class Home extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        );
+    }
+
+}
+
+export default Home;
