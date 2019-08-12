9e679f780efab80afa077e2bcae42e548af49a50
xiehongwei <283994917@qq.com>
2019-08-12 17:19:18 +0800

    init start code

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, {Component, Fragment} from 'react';
+// import logo from './logo.svg';
+// <img src={logo} className="App-logo" alt="logo" />
+
+
+
+
+class App extends Component {
+
+
+  render() {
+    return (
+        <div>
+          Start Code
+        </div>
+    );
+  }
+
+}
+
+
+export default App;
