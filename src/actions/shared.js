76f2b74bf9a794a486bca6ce8760aa9e2c1a097c
xiehongwei <283994917@qq.com>
2019-08-12 18:13:51 +0800

    handleInitData 初始化数据

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,1 +1,24 @@
+import {getInitData} from "../utils/api";
+import {setAuthUser} from "./authUser";
+import {receiveQuestions} from "./questions";
+import {receiveUsers} from "./users";
+import {showLoading,hideLoading} from "react-redux-loading";
 
+
+const USER = 'tylermcginnis';
+
+export function handleInitData () {
+    return (dispatch) => {
+        dispatch(showLoading());
+
+        return getInitData()
+            .then(
+                ({ users, questions }) => {
+                    dispatch(receiveUsers(users));
+                    dispatch(receiveQuestions(questions));
+                    dispatch(setAuthUser(USER));
+                    dispatch(hideLoading());
+                }
+            )
+    }
+}
