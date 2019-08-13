254c29dee32ef440f852a44e6a058ef5a7025cf6
xiehongwei <283994917@qq.com>
2019-08-13 14:58:29 +0800

    初始完成

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,5 +1,4 @@
 import {getInitData} from "../utils/api";
-import {setAuthUser} from "./authUser";
 import {receiveQuestions} from "./questions";
 import {receiveUsers} from "./users";
 import {showLoading,hideLoading} from "react-redux-loading";
@@ -55,7 +54,6 @@ export function handleInitData () {
                 ({ users, questions }) => {
                     dispatch(receiveUsers(users));
                     dispatch(receiveQuestions(questions));
-                    dispatch(setAuthUser(USER));
                     dispatch(hideLoading());
                 }
             )
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,82 @@
 import React, {Component} from 'react';
-
-
+import { connect } from 'react-redux';
+import {setAuthUser} from "../actions/authUser";
+import { withRouter } from 'react-router-dom';
 
 
 class Login extends Component {
 
 
+    state = {
+        value: ''
+    };
+
+    handleChange = (value) => {
+        this.setState({value})
+    };
+
+    handleSubmit = () => {
+        if (this.state.value !== '') {
+            this.props.setAuthUser(this.state.value);
+            this.props.history.push('/')
+        }
+    };
+
+
     render() {
+
+        const { authUser, users } = this.props;
+        const { value } = this.state;
+
+
+
         return (
             <div>
-                Login
+                <header>
+                    <h1>Welcome to the would you rather App!</h1>
+                    <p>Please sign in to continue</p>
+                </header>
+                <div>
+                    <img src="/images/logo.svg" alt="logo"/>
+                </div>
+
+                <div>
+                    <select value={value}
+                            onChange={(e) => this.handleChange(e.target.value)}
+                            >
+                        <option value="">Choose a User</option>
+                        {users.length > 0 &&
+                        users.map((user) => (
+                            <option value={user.id} key={user.id}>{user.name}</option>
+                        ))}
+                    </select >
+                    <button type="submit"
+                            onClick={this.handleSubmit}
+                            disabled={!value}
+                    >
+                        Login
+                    </button>
+                </div>
             </div>
         );
     }
 
 }
 
-export default Login;
+
+
+function mapStateToProps ({ authUser, users }) {
+    return {
+        authUser,
+        users:Object.values(users)
+    }
+}
+
+
+
+export default withRouter(
+    connect(
+        mapStateToProps,
+        { setAuthUser }
+    )(Login)
+)
