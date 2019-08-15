1ac8fb6dc0c263b166821b83fa580901e3e95922
xiehongwei <283994917@qq.com>
2019-08-15 14:05:01 +0800

    将UseCard 转为无状态组件

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,4 +1,4 @@
-import React, {Component,Fragment} from 'react';
+import React, {Fragment} from 'react';
 import {NavLink} from "react-router-dom";
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
@@ -10,68 +10,63 @@ import {
 } from "semantic-ui-react";
 
 
-class UserCard extends Component {
+const UserCard = (props) => {
 
+    const { users, question, question_type } = props;
 
-    render() {
-
-        const { users, question, question_type } = this.props;
-
-        if (question === undefined) {
-            return <Redirect to="/questions/nomatch" />;
-        }
-
-        return (
-            <Fragment>
-                <Grid celled>
-                    <Grid.Row color="grey">
-                        <Grid.Column width={16} textAlign="left">
-                            {users[question.author].name} asks:
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row>
-                        <Grid.Column width={5}>
-                            <Image src={users[question.author].avatarURL} />
-                        </Grid.Column>
-                        <Grid.Column width={11}>
-                            <Header as="h5" textAlign="left">
-                                Would you rather
-                            </Header>
-                            <p>
-                                {question.optionOne.text}...
-                                <br />
-                                or...
-                            </p>
-                            {
-                                question_type === 'unanswered' && (
-                                    <Button
-                                        as={NavLink}
-                                        to={`/questions/${question.id}`}
-                                        content='View Poll'
-                                        size="tiny"
-                                        fluid positive
-                                    />
-                                )
-
-                            }
-
-                            {
-                                question_type === 'answered' && (
-                                    <Button
-                                        as={NavLink}
-                                        to={`/results/${question.id}`}
-                                        content='View Answer'
-                                        size="tiny"
-                                        fluid positive
-                                    />
-                                )
-                            }
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </Fragment>
-        );
+    if (question === undefined) {
+        return <Redirect to="/questions/nomatch" />;
     }
+    return (
+        <Fragment>
+            <Grid celled>
+                <Grid.Row color="grey">
+                    <Grid.Column width={16} textAlign="left">
+                        {users[question.author].name} asks:
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={5}>
+                        <Image src={users[question.author].avatarURL} />
+                    </Grid.Column>
+                    <Grid.Column width={11}>
+                        <Header as="h5" textAlign="left">
+                            Would you rather
+                        </Header>
+                        <p>
+                            {question.optionOne.text}...
+                            <br />
+                            or...
+                        </p>
+                        {
+                            question_type === 'unanswered' && (
+                                <Button
+                                    as={NavLink}
+                                    to={`/questions/${question.id}`}
+                                    content='View Poll'
+                                    size="tiny"
+                                    fluid positive
+                                />
+                            )
+
+                        }
+
+                        {
+                            question_type === 'answered' && (
+                                <Button
+                                    as={NavLink}
+                                    to={`/results/${question.id}`}
+                                    content='View Answer'
+                                    size="tiny"
+                                    fluid positive
+                                />
+                            )
+                        }
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Fragment>
+    );
 
 }
 
